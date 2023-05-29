5e29a60a0d126e787973a64fbdb0870d576fa6b8
markusleh <28872014+markusleh@users.noreply.github.com>
2023-05-30 00:07:45 +0300

    add vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
 import { ThemeProvider } from "@/components/theme-provider"
+import { Analytics } from '@vercel/analytics/react';
+
 
 export const metadata: Metadata = {
   title: {
@@ -47,6 +49,7 @@ export default function RootLayout({ children }: RootLayoutProps) {
             </div>
             <TailwindIndicator />
           </ThemeProvider>
+          <Analytics />
         </body>
       </html>
     </>
